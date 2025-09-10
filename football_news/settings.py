fa09b5346486e56659b3affea040b6b7de184f9f
azzelll <madeshandykrisnanda@gmail.com>
2025-09-10 15:56:45 +0700

    update SCRF

--- football_news/settings.py
+++ football_news/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "made-shandy-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "<https://made-shandy-footballnews.pbp.cs.ui.ac.id/"
+    "https://made-shandy-footballnews.pbp.cs.ui.ac.id/"
 ]
 # Application definition
 
